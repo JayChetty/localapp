6de6501f210937af0268b31b910ea40811542f91
JayChetty <jaypaulchetty@gmail.com>
2014-04-26 23:40:49 +0100

    change user to owner

--- db/migrate/20140426223843_devise_create_owners.rb
+++ db/migrate/20140426223843_devise_create_owners.rb
@@ -1,6 +1,6 @@
-class DeviseCreateUsers < ActiveRecord::Migration
+class DeviseCreateOwners < ActiveRecord::Migration
   def change
-    create_table(:users) do |t|
+    create_table(:owners) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -34,9 +34,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
-    add_index :users, :reset_password_token, :unique => true
-    # add_index :users, :confirmation_token,   :unique => true
-    # add_index :users, :unlock_token,         :unique => true
+    add_index :owners, :email,                :unique => true
+    add_index :owners, :reset_password_token, :unique => true
+    # add_index :owners, :confirmation_token,   :unique => true
+    # add_index :owners, :unlock_token,         :unique => true
   end
 end
